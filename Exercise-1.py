ac3bdaf5d9062b2daa7a8316cf8339160f0b6cca
JoshuaMina <joshua.minacambronero@gmail.com>
2023-01-15 01:29:54 +0800

    commit

--- Exercise-1.py
+++ Exercise-1.py
@@ -6,10 +6,13 @@ class BinarySearchTreeNode:
 
     def add_child(self, data):
         if data == self.data:
-
+            return
 
         if data < self.data:
             if self.left:
                 self.left.add_child(data)
             else:
                 self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
