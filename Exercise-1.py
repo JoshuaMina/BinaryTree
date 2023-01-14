425ec433e22f458e5e5f2d87e4e2fb117f2f8f14
JoshuaMina <joshua.minacambronero@gmail.com>
2023-01-15 01:56:56 +0800

    commit

--- Exercise-1.py
+++ Exercise-1.py
@@ -74,3 +74,6 @@ class BinarySearchTreeNode:
         left_sum = self.left.calculate_sum() if self.left else 0
         right_sum = self.right.calculate_sum() if self.right else 0
         return self.data + left_sum + right_sum
+
+    def build_tree(elements):
+         root = BinarySearchTreeNode(elements[0])
