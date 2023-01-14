26f12b9919b25ddd6664cbe18cb63f5b714fcd67
JoshuaMina <joshua.minacambronero@gmail.com>
2023-01-15 01:34:30 +0800

    commit

--- Exercise-1.py
+++ Exercise-1.py
@@ -28,3 +28,8 @@ class BinarySearchTreeNode:
                 return self.left.search(val)
             else:
                 return False
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
