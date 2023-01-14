f79578bc7d49172b9efeac150b41cdc70aec3ab4
JoshuaMina <joshua.minacambronero@gmail.com>
2023-01-15 01:40:41 +0800

    commit

--- Exercise-1.py
+++ Exercise-1.py
@@ -45,3 +45,5 @@ class BinarySearchTreeNode:
         elements = []
         if self.left:
             elements += self.left.post_order_traversal()
+        if self.right:
+            elements += self.right.post_order_traversal()
